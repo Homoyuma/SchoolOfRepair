94c544d08d9f8e7321ecf9ced3578de1799f5aeb
Homoyuma <stepanovice14@gmail.com>
2020-10-30 17:17:04 +0900

    Ava TD ver 0.1

---
+++ Tower Defense/Assets/Scripts/MobSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawn : MonoBehaviour
+{
+    public int WaveSize;
+    public GameObject EnemyPrefab;
+    public float EnemyInterval;
+    public Transform spawnPoint;
+    public float startTime;
+    public Transform[] WayPoints;
+    public string gateTag;
+    int enemyCount = 0;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", startTime, EnemyInterval);
+    }
+
+    void Update()
+    {
+        if (enemyCount == WaveSize)
+        {
+            CancelInvoke("SpawnEnemy");
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        enemyCount++;
+        GameObject enemy = GameObject.Instantiate(EnemyPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
+        enemy.GetComponent<MoveToWayPoint>().waypoints = WayPoints;
+    }
+}
---
+++ Tower Defense/Assets/Scripts/MoveToWayPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToWayPoint : MonoBehaviour
+{
+    public float Speed;
+    public Transform[] waypoints;
+    int curWaypointIndex = 0;
+
+    void Suicide()
+    {
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        if (curWaypointIndex < waypoints.Length)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[curWaypointIndex].position, Time.deltaTime * Speed);
+            if (Vector2.Distance(transform.position, waypoints[curWaypointIndex].position) < 0.5f)
+            {
+                curWaypointIndex++;
+            }
+        }
+        else {
+            Suicide();
+        }
+    }
+}
