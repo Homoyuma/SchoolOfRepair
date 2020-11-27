71a389e1c522aca9adfd631ae7cd42a6d6fbaeb4
Homoyuma <stepanovice14@gmail.com>
2020-11-27 16:48:19 +0900

    lose/win fix

--- Tower Defense/Assets/Scripts/MobSpawn.cs
+++ Tower Defense/Assets/Scripts/MobSpawn.cs
@@ -1,36 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MobSpawn : MonoBehaviour
 {
-    public int WaveSize;
     public GameObject EnemyPrefab;
-    public float EnemyInterval;
+    private float countdown = 2f;
+    public float timeBetweenWaves;
+    private int waveIndex = 1;
     public Transform spawnPoint;
-    public float startTime;
     public Transform[] WayPoints;
-    public string gateTag;
     public static int enemyCount = 0;
     public static int enemiesAlive = 0;
     public manager mng;
 
     void Start()
     {
-        
-        InvokeRepeating("SpawnEnemy", startTime, EnemyInterval);
+        enemyCount = 0;
     }
 
     void Update()
     {
-        if (enemyCount == WaveSize)
+        /*
+        if (enemiesAlive == 0 && enemyCount == 10 && PlayerStats.Lives > 0)
         {
-            CancelInvoke("SpawnEnemy");
-
+  
+        }*/
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
         }
+        countdown -= Time.deltaTime;
         if (enemiesAlive == 0 && enemyCount == 10 && PlayerStats.Lives > 0)
         {
             mng.WinLevel();
+        }
+        }
+    IEnumerator SpawnWave()
+    {
+        while (enemyCount != 10) {
+            for (int i = 0; i < waveIndex; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(0.5f);
+            }
         }
     }
 
--- Tower Defense/Assets/Scripts/manager.cs
+++ Tower Defense/Assets/Scripts/manager.cs
@@ -21,8 +21,6 @@ public class manager : MonoBehaviour
         {
             EndGame();
         }
-
-
     }
     void EndGame()
     {
