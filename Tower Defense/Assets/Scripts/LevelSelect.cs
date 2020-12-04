37678ed2a5a9ffc6498d52b17023c0bb3437c59b
Aleks1999 <akychkin15@gmail.com>
2020-12-04 15:23:29 +0900

    LevelSelect

---
+++ Tower Defense/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void Level1Button ()
+    {
+        SceneManager.LoadScene ("SampleScene");
+    }
+    public void Level2Button ()
+    {
+        Debug.Log ("There is no level 2!");
+    }
+}
